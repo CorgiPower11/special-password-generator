411b75a0925592d9280bc0df322b59239197b74f
Kathy Moua <kathy@page-ed.org>
2022-01-16 22:38:13 -0600

    window popup and add README.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# special-password-generator
+# Special Password Generator
+
+## Description
+The objective of this project is to develop a password generator. Users can choose the length of the password and confirm if the password should include lowercase letters, uppercase letters, numbers, or special charaters.
+
+## Usage
+Below is a screenshot of the landing page. The link to deploy the application is https://github.com/CorgiPower11/special-password-generator. Click here for the webpage: https://corgipower11.github.io/special-password-generator/.
+    ![Landing Page Screenshot](assets\images\Landing-pages-screenshot.jpg)
--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-// Assignment code here
+// 1. Choose a length of at least 8 characters and no more than 128 characters
+// 2. Confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// 3. Generate password
+// 4. Display password to page.
 
+// Assignment code here
+const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var length = window.prompt("How many chararters would you like your password to contain? Max: 128 Charaters")
+  var lowercase = window.confirm("Click OK to include lowercase letters.")
+  var uppercase = window.confirm("Click OK to include uppercase letters.")
+  var numbers = window.confirm("Click OK to include Numbers.")
+  var specialCharaters = window.confirm("Click OK to include special charaters.")
 
+  var password = generatePassword(length.value, Charaters);
+  var passwordText = document.querySelector("#password");
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    password += charaters.charAt (
+      Math.floor(Math.random() * length)
+    )
+  }
+
+  return password ;
+}
